61b43ab1f278e7a140cbc19b7fe010d8209a6621
shhanson <sarahh.hanson@gmail.com>
2017-09-15 11:09:12 -0500

    Added basic component and state scaffolding

--- public/js/meetrunnerApp.js
+++ public/js/meetrunnerApp.js
@@ -11,6 +11,56 @@
         name: 'splash',
         url: '/',
         component: 'splash',
+      })
+      .state({
+        name: 'loginReg',
+        url: '/login',
+        component: 'loginReg',
+      })
+      .state({
+        name: 'profile',
+        url: '/users/:user_id',
+        component: 'profile',
+      })
+      .state({
+        name: 'allEvents',
+        url: '/events',
+        component: 'allEvents',
+      })
+      .state({
+        name: 'myEvents',
+        url: '/users/:user_id/events',
+        component: 'myEvents',
+      })
+      .state({
+        name: 'event',
+        url: '/events/:id',
+        component: 'event',
+      })
+      .state({
+        name: 'eventReg',
+        url: '/events/:event_id/register',
+        component: 'eventReg',
+      })
+      .state({
+        name: 'createEvent',
+        url: '/events/new',
+        component: 'createEvent',
+      })
+      .state({
+        name: 'session',
+        url: '/events/:event_id/sessions/:session_id',
+        component: 'session',
+      })
+      .state({
+        name: 'createSession',
+        url: '/events/:id/sessions/new',
+        component: 'createSession',
+      })
+      .state({
+        name: 'athletes',
+        url: '/events/:id/athletes',
+        component: 'athletes',
       });
   }
 }());
