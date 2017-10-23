29077be0d978756f9f07d2be51d7688b0d15e637
shhanson <sarahh.hanson@gmail.com>
2017-10-23 11:16:33 -0500

    Compressed images

--- migrations/20170924182828_add_image_url_to_events.js
+++ migrations/20170924182828_add_image_url_to_events.js
@@ -1,6 +1,6 @@
 exports.up = knex =>
   knex.schema.table('events', (table) => {
-    table.string('image_url').defaultTo('/images/karhu_platform.jpg');
+    table.string('image_url').defaultTo('/images/karhu_platform-min.jpg');
   });
 
 exports.down = knex =>
