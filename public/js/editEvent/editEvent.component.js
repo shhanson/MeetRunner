e07ec177121a64d3a699b2ef16cb9ff621e97f5b
shhanson <sarahh.hanson@gmail.com>
2017-10-24 12:25:36 -0500

    Added form validation to editEvent page

--- public/js/editEvent/editEvent.component.js
+++ public/js/editEvent/editEvent.component.js
@@ -15,15 +15,23 @@
       EventsService.getEvent(vm.eventID).then(() => {
         vm.event = EventsService.event;
         vm.event.entry_fee = (vm.event.entry_fee_cents / 100).toFixed(2);
+
+        vm.event.start_date = new Date(vm.event.start_date);
+        vm.event.end_date = new Date(vm.event.end_date);
       });
     };
 
     vm.updateEvent = function updateEvent() {
       vm.event.entry_fee_cents = vm.event.entry_fee * 100;
+      vm.event.start_date = new Date(vm.event.start_date).toISOString();
+      vm.event.end_date = new Date(vm.event.end_date).toISOString();
+
       EventsService.updateEvent(vm.eventID, vm.event).then(() => {
         vm.event = EventsService.event;
         $state.go('eventReg', { event_id: vm.eventID });
       });
     };
+
+
   }
 }());
